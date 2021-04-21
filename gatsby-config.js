f7fb0fd8a3f2fb9c6fb2ba8b0c61ae1d14718ac9
hugo-a-garcia <hugo.a.garcia@gmail.com>
2021-04-21 08:40:21 +0200

    Editing default generated files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    // Used for the site title and SEO
+    title: `BlueCorn Blog`,
+    // Used to provide alt text for your avatar
+    author: `Hugo`,
+    // Used for SEO
+    description: `A Journey into Robotics`,
+    // Used for resolving images in social cards
+    siteUrl: `http://bluecorn.dev`,
+    // Used for social links in the root footer
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/bluecorn`,
+      },
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +42,14 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+        prismPreset: `prism-okaidia`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
